175f7f8a9456af377bdaca0a9f273c366d3a3cf6
Gambest <gomesgabriel@id.uff.br>
2025-03-11 02:11:22 -0300

    update: working topKFrequent347

--- TopKFrequent347.java
+++ TopKFrequent347.java
@@ -1,71 +1,43 @@
 import java.util.*;
-import java.util.function.IntFunction;
 
 public class TopKFrequent347 {
     public static void main(String[] args) {
-        int[] nums = {-1,-1};
+        int[] nums = {4,1,-1,2,-1,2,3};
 
-        topKFrequent(nums, 1);
+        System.out.println(Arrays.toString(topKFrequent(nums, 2)));
     }
 
-    public static int[] topKFrequent(int[] nums, int k) {
-        if (nums.length == k) return nums;
+    public static int[] topKFrequent(int[] nums, int k){
+        Map<Integer, Integer> numberFrequencyMap = new HashMap<>();
 
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int num:nums){
-            if (frequencyMap.get(num)==null){
-                frequencyMap.put(num,1);
-                continue;
+        for (int num : nums) {
+            Integer put = numberFrequencyMap.put(num, 1);
+            if (put != null) {
+                numberFrequencyMap.put(num, put + 1);
             }
-            frequencyMap.put(num, frequencyMap.get(num) + 1);
-        }
-        System.out.println(frequencyMap.size());
-        if (frequencyMap.size()==k){
-            int[] kFrequency = new int[k];
-            List<Integer> kFrequentsList = new ArrayList<>(frequencyMap.keySet());
-
-            for (int i = 0; i<k;i++){
-                kFrequency[i] = kFrequentsList.get(i);
-                System.out.println(kFrequency[i]);
-                System.out.println(kFrequentsList.get(i));
-            }
-            return kFrequency;
         }
 
-        System.out.println(frequencyMap);
+        List<Integer> numberList = new ArrayList<>(numberFrequencyMap.keySet());
+        List<Integer> topKFrequentList = new ArrayList<>();
+        int frequency = 0;
+        int frequencyNumber = 0;
 
-        List<Integer> frequencyList = new ArrayList<>(frequencyMap.values());
-        frequencyList.sort(Comparator.naturalOrder());
-
-        System.out.println(frequencyList);
-
-        int[] higherFrequencies = new int[k];
-        for (int i=frequencyList.size() - 1; i>frequencyList.size()-k-1;i--){
-            higherFrequencies[i-1]=frequencyList.get(i);
-            System.out.println(higherFrequencies[i-1]);
-        }
-
-        List<Integer> kFrequentsList = new ArrayList<>();
-
-        for (Integer num:frequencyMap.keySet()){
-            Integer frequency = frequencyMap.get(num);
-            for (int i = 0; i<k;i++){
-                if (frequency == higherFrequencies[i]){
-                    kFrequentsList.add(num);
-                    System.out.println("Added:" + num);
+        for (int i = 0; i < k ;i++) {
+            for (Integer number : numberList) {
+                if (frequency < numberFrequencyMap.get(number)) {
+                    frequency = numberFrequencyMap.get(number);
+                    frequencyNumber = number;
                 }
             }
+            System.out.println("Added: " + frequencyNumber);
+            topKFrequentList.add(frequencyNumber);
+            numberList.remove(topKFrequentList.get(i));
+            frequency = 0;
+            System.out.println("i : " + i);
         }
 
-        System.out.println(kFrequentsList);
-        int[] kFrequency = new int[k];
+        Integer[] topK = topKFrequentList.toArray(new Integer[k]);
 
-        for (int i = 0; i<k;i++){
-            kFrequency[i] = kFrequentsList.get(i);
-            System.out.println(kFrequency[i]);
-            System.out.println(kFrequentsList.get(i));
-        }
-
-        return kFrequency;
+        return Arrays.stream(topK).mapToInt(Integer::intValue).toArray();
     }
 }
